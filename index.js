502a0a0abe457b78a92c51c6d26f5527fa41f3cd
royrv <roy.rojval@gmail.com>
2018-11-16 18:56:27 -0500

    stop propagation

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 
 var app = new Vue({
-    el: '#app',
+  el: '#app',
 
-    data: {
-      primerMensaje: 'Ejemplo.js!',
-      segundoMensaje:'',
-      area : 'Este es el area',
-      isSelected : false,
-      listOfCheckboxes : [],
-      listOfRadioButton: '',
-      selectedDropDownOption : '',
-      selectedDropDownOptionMultiple : [
-        'opcionMultiple1' , 'opcionMultiple2'
-      ],
-      groceryList: [
-        { id:0, text: 'Learn JavaScript' },
-        { id:1,text: 'Learn Vue' },
-        { id:2,text: 'Build something awesome' }
-      ],
-      inputValue : ''
-    } ,
+  data: {
+    primerMensaje: 'Ejemplo.js!',
+    segundoMensaje: '',
+    area: 'Este es el area',
+    isSelected: false,
+    listOfCheckboxes: [],
+    listOfRadioButton: '',
+    selectedDropDownOption: '',
+    selectedDropDownOptionMultiple: [
+      'opcionMultiple1', 'opcionMultiple2'
+    ],
+    groceryList: [
+      { id: 0, text: 'Learn JavaScript' },
+      { id: 1, text: 'Learn Vue' },
+      { id: 2, text: 'Build something awesome' }
+    ],
+    inputValue: ''
+  },
 
-    methods: {
-        cambiaText : cambiaText,
-        executeSearch: executeSearch
-    }
-  }) 
-  function cambiaText(texto,event){
-    this.primerMensaje = texto;
+  methods: {
+    cambiaText: cambiaText,
+    executeSearch: executeSearch,
+    grandparentStopClick: grandparentStopClick,
+    parentStopClick:parentStopClick,
+    executeStopSearch : executeStopSearch
   }
-  function executeSearch(){
-    console.log("executeSearch");
-    if(this.inputValue){
-      console.log("if");
-      document.forms[0].submit();
-    }
-    else {
-      console.log("else")
-    }
+})
+function cambiaText(texto, event) {
+  this.primerMensaje = texto;
+}
+function executeSearch() {
+  console.log("executeSearch");
+  if (this.inputValue) {
+    console.log("if");
+    document.forms[0].submit();
   }
+  else {
+    console.log("else")
+  }
+}
+function executeStopSearch (){
+  console.log("executeStopSearch");
+}
+function parentStopClick (){
+  console.log("parentStopClick");
+}
+function grandparentStopClick (){
+  console.log("grandparentStopClick");
+}
