f9ac5942b8b870b2bdef41272fa3ed561f4ac8c1
royrv <roy.rojval@gmail.com>
2018-11-19 03:14:11 -0500

    adding tuto 3 - adding vue cli

---
+++ tuto3/build-a-bot/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png" />
+            </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+    <dashboard></dashboard>
+    </main>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import Dashboard from './components/Dashboard.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Dashboard,
+    // HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+main  {
+  margin:  0,auto;
+  padding: 30px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
---
+++ tuto3/build-a-bot/src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img class="robot" src="../assets/robot-home.png" />
+        <h1>dashboard</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+.home {
+    text-align: 30px;
+}
+.robot {
+    height: 300px;
+}
+</style>
